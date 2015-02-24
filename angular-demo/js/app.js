cac4b11eede121cc385ee22e0c687a554415202d
13scoobie <13scoobie@gmail.com>
2015-02-23 20:38:13 -0800

    Updated Right hand content (temporary)

--- angular-demo/js/app.js
+++ angular-demo/js/app.js
@@ -2,19 +2,26 @@ var app = angular.module("leftHandNav", []);
 app.controller("leftNavController", function($scope) {
     $scope.navigation = [
         {title: "LinkedIn", href: "https://www.linkedin.com/pub/david-white/41/511/7b8/", description: ""},
-        {title: "GitHub", href: "http://13scoobie.github.io", description: ""},
-        {title: "Stack Overflow", href: "http://stackoverflow.com/users/3565567/user3565567", description: ""}
+        {title: "GitHub", href: "https://github.com/13scoobie", description: ""},
+        {title: "Stack Overflow", href: "http://stackoverflow.com/users/3565567/user3565567", description: ""},
+        {title: "Website", href: "http://13scoobie.github.io/", description: ""},
+        {title: "Blog", href: "http://13scoobie.github.io/poole/", description: ""}
     ];
 });
 
 app.controller("rightHandController", function($scope) {
     $scope.rightContent = [
-        {title: "1", href: "test.html", description:  "test description 1"},
-        {title: "2", href: "test.html",  description: "test description 2"},
-        {title: "3", href: "test.html",  description: "test description 3"},
-        {title: "4", href: "test.html",  description: "test description 4"},
-        {title: "5", href: "http.html",  description: "test description 5"},
-        {title: "6", href: "http.html",  description: "test description 6"},
-        {title: "7", href: "http.html",  description: "test description 7"}
+        {title: "Project Euler", href: "#", description:  "test description 1"},
+        {title: "Hacker Rank", href: "#",  description: "test description 2"},
+        {title: "Over the Wire", href: "#",  description: "test description 3"},
+        {title: "Unity Game Development (tbd)", href: "#",  description: "test description 6"},
+        {title: "Windows Phone Development (tbd)", href: "#",  description: "test description 7"},
+        {title: "Windows 10 Development (tbd)", href: "#",  description: "test description 7"},
+        {title: "Android Development (tbd)", href: "#",  description: "test description 7"},
+        {title: "iOS Development (tbd)", href: "#",  description: "test description 7"},
+        {title: "Movies (tbd)", href: "#",  description: "test description 4"},
+        {title: "Xbox Games (tbd)", href: "#",  description: "test description 5"}
+        {title: "Apps I Use", href: "#",  description: "test description 4"},
+        {title: "Blogs I Read", href: "#",  description: "test description 5"},
     ];
 });
