6086488391f384bff10cd90f3de27f2bb3188612
13scoobie <13scoobie@gmail.com>
2016-09-26 20:04:02 -0700

    add timestamp, order descending

--- blog/blog.js
+++ blog/blog.js
@@ -3,7 +3,7 @@ app.controller("leftNavController", function($scope) {
     $scope.navigation = [
         {title: "LinkedIn", href: "https://www.linkedin.com/pub/david-white/41/511/7b8/", description: ""},
         {title: "GitHub", href: "https://github.com/13scoobie", description: ""},
-        {title: "Twitter", href: "https://twitter.com/13scoobie", description: ""},        
+        {title: "Twitter", href: "https://twitter.com/13scoobie", description: ""},
         {title: "Stack Overflow", href: "http://stackoverflow.com/users/3565567/user3565567", description: ""},
         {title: "Website", href: "http://13scoobie.github.io/", description: ""},
         {title: "Blog", href: "http://13scoobie.github.io/blog/", description: ""}
@@ -12,8 +12,8 @@ app.controller("leftNavController", function($scope) {
 
 app.controller("rightHandController", function($scope) {
     $scope.rightContent = [
-        {title: "First Blog Post", href: "pages/09052016-first-blog-post.html", description:  "First Blog post - description to come later (in progress)", publishdate: "09/05/2016"},
-        {title: "Second Blog Post", href: "pages/09052016-fix-static-site.html", description:  "Things i need to change to this website", publishdate: "09/05/2016"},
-        {title: "The Plan...", href: "pages/09262016-the-plan.html", description: "I plan to read 1 book a week and make atleast 1 commit a day to this blog", publishdate: "09/26/2016"}
+        {title: "First Blog Post", href: "pages/09052016-first-blog-post.html", description:  "First Blog post - description to come later (in progress)", publishdate: "09/05/2016 20:00:00"},
+        {title: "Second Blog Post", href: "pages/09052016-fix-static-site.html", description:  "Things i need to change to this website", publishdate: "09/05/2016 20:05:00"},
+        {title: "The Plan...", href: "pages/09262016-the-plan.html", description: "I plan to read 1 book a week and make atleast 1 commit a day to this blog", publishdate: "09/26/2016 20:01:50"}
         ];
 });
--- sub-page-js/app_AppsIUse.js
+++ sub-page-js/app_AppsIUse.js
@@ -5,7 +5,7 @@ app_AppsIUse.controller("leftNavController", function($scope) {
         {title: "Google Chrome", href: "#GoogleChrome", description: "Browser"},
         {title: "Firefox", href: "#Firefox", description: "Browser"},
         {title: "Firebug", href: "#Firebug", description: "Browser Extension"},
-		{title: "Visual Studio", href: "#VisualStudio", description: "Development IDE"},
+		    {title: "Visual Studio", href: "#VisualStudio", description: "Development IDE"},
         {title: "Back to Homepage", href: "http://13scoobie.github.io"}
     ];
 });
@@ -22,12 +22,12 @@ app_AppsIUse.controller("rightHandController", function($scope) {
         }
         $scope.showAll = true;
     };
-    
+
    $scope.myFunc = function(a) {
        if($scope.showAll) { return true; }
-       
+
        var sel = false;
-       
+
         for(service in $scope.servicesArray){
             var t = $scope.servicesArray[service];
             console.log(t);
@@ -37,7 +37,7 @@ app_AppsIUse.controller("rightHandController", function($scope) {
                 }else{
                     sel = true;
                 }
-            }           
+            }
         }
        return sel;
     };
@@ -49,50 +49,50 @@ app_AppsIUse.controller("rightHandController", function($scope) {
 		{ name: "Mobile Development", on: false},
 		{ name: "Web Development", on: false},
 		{ name: "Windows Development", on: false},
-		{ name: "Internet Surfing", on: false}				
+		{ name: "Internet Surfing", on: false}
 	];
 
     $scope.rightContent = [
         {
-            appName: "GoogleChrome", 
-            href: "https://www.google.com/chrome/", 
-            description: "Browser released by Google", 
+            appName: "GoogleChrome",
+            href: "https://www.google.com/chrome/",
+            description: "Browser released by Google",
             "technology": [
                 "Web Development"
             ]
         },
         {
-            appName: "Firefox", 
-            href: "https://www.mozilla.org/en-US/firefox/new/", 
-            description: "Browser released by Mozilla team", 
+            appName: "Firefox",
+            href: "https://www.mozilla.org/en-US/firefox/new/",
+            description: "Browser released by Mozilla team",
             "technology": [
                 "Web Development"
             ]
         },
         {
-            appName: "Firebug", 
-            href: "getfirebug.com/", 
-            description: "Extension that comes in handy for web development (for firefox)", 
+            appName: "Firebug",
+            href: "getfirebug.com/",
+            description: "Extension that comes in handy for web development (for firefox)",
             "technology": [
                 "Web Development"
             ]
-        },        
+        },
 		{
-			appName: "Sublime", 
-			href: "http://www.sublimetext.com", 
-			description: "Text editor used for writing code quickly", 
+			appName: "Sublime",
+			href: "http://www.sublimetext.com",
+			description: "Text editor used for writing code quickly",
 			"technology": [
 				"Web Development"
 			]
 		},
 		{
-			appName: "VisualStudio", 
-			href: "http://www.visualstudio.com/en-us/products/visual-studio-community-vs.aspx", 
-			description: "My Main IDE for c#/ASP.net Development", 
+			appName: "VisualStudio",
+			href: "http://www.visualstudio.com/en-us/products/visual-studio-community-vs.aspx",
+			description: "My Main IDE for c#/ASP.net Development",
 			"technology": [
 				"Web Development", "Windows Development"
 			]
 		},
-		
+
 	];
-});	
+});
