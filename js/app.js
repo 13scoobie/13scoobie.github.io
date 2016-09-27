bc27acb6bb8623e5a120c064aab5f255c2cb67af
13scoobie <13scoobie@gmail.com>
2016-09-26 19:58:53 -0700

    add twitter link to left hand nav

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ app.controller("leftNavController", function($scope) {
     $scope.navigation = [
         {title: "LinkedIn", href: "https://www.linkedin.com/pub/david-white/41/511/7b8/", description: ""},
         {title: "GitHub", href: "https://github.com/13scoobie", description: ""},
+        {title: "Twitter", href: "https://twitter.com/13scoobie", description: ""},
         {title: "Stack Overflow", href: "http://stackoverflow.com/users/3565567/user3565567", description: ""},
         {title: "Website", href: "http://13scoobie.github.io/", description: ""},
         {title: "Blog", href: "http://13scoobie.github.io/blog/", description: ""}
