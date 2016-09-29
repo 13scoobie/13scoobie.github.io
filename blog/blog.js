9e840d41a3912b9d91eb5ee3f6da50db0978da58
13scoobie <13scoobie@gmail.com>
2016-09-28 20:25:58 -0700

    pragmatic programmer blog entry 9/28

--- blog/blog.js
+++ blog/blog.js
@@ -15,6 +15,7 @@ app.controller("rightHandController", function($scope) {
         {title: "First Blog Post", href: "pages/09052016-first-blog-post.html", description:  "First Blog post - description to come later (in progress)", publishdate: "09/05/2016 20:00:00"},
         {title: "Second Blog Post", href: "pages/09052016-fix-static-site.html", description:  "Things i need to change to this website", publishdate: "09/05/2016 20:05:00"},
         {title: "The Plan...", href: "pages/09262016-the-plan.html", description: "I plan to read 1 book a week and make atleast 1 commit a day to this blog", publishdate: "09/26/2016 20:01:50"},
-        {title: "The New Plan...", href: "pages/09272016-new-plan.html", description: "Changing to one book a month, and adding 1 language a month", publishdate: "09/27/2016 21:06:30"}
+        {title: "The New Plan...", href: "pages/09272016-new-plan.html", description: "Changing to one book a month, and adding 1 language a month", publishdate: "09/27/2016 21:06:30"},
+        {title: "The First Book", href: "pages/09282016-first-book.html", description: "Started my first book, 'Pragmatic Programmer from Journeyman to Master', so far, so good.", publishdate: "09/28/2016 20:26:00"}
         ];
 });
