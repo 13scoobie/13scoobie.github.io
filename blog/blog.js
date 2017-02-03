081de1bf4ed40724bf009638ad9461f0c0970c73
13scoobie <13scoobie@gmail.com>
2017-02-03 15:20:36 -0800

    update to blog

--- blog/blog.js
+++ blog/blog.js
@@ -20,6 +20,9 @@ app.controller("rightHandController", function($scope) {
         {title: "React & Angular", href: "pages/09292016-react-and-angular.html", description: "Start diving into React and Angular 2.0, write some code in both and see what each has to offer", publishdate: "09/29/2016 20:41:00"},
         {title: "Google Hardware (Pixel/Andromeda)", href: "pages/10032016-google-hardware.html", description: "Google Hardware event tomorrow!", publishdate: "10/03/2016 20:24:00"},
         {title: "Google Pixel Phones", href: "pages/10042016-google-pixel-phones.html", description: "Google Pixel Phones", publishdate: "10/04/2016 20:07:00"},
-        {title: "First Challenge", href: "pages/10062016-first-challenge-done.html", description: "First Programming Challenge complete - Left Shift Array", publishdate: "10/06/2016 21:03:00"}
+        {title: "First Challenge", href: "pages/10062016-first-challenge-done.html", description: "First Programming Challenge complete - Left Shift Array", publishdate: "10/06/2016 21:03:00"},
+        {title: "New Year - New Post", href: "pages/01262017-new-year-new-post.html", description: "Welcome to 2017 - It's a new year, lots of fun things to look forward to", publishdate: "01/26/2017 15:54:00"},
+        {title: "Blog updates and new upcoming topics", href: "pages/01262017-upcoming-updates.html", description: "Lots of stuff in the pipeline, new technology, more insight, what am i using? what projects am i wanting to dive into", publishdate: "01/26/2017 15:59:00"},
+        {title: "Why am I blogging/programming?", href: "pages/01262017-why-blog.html", description: "Find out why I'm blogging (i should make a ramblings section)", publishdate: "01/26/2017 16:11:00"}
         ];
 });
